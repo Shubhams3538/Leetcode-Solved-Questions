f0cf7c7205e8f6af26d65c3741a06c4a0513b91b
Shubham kumar singh <Shubhams3538@gmail.com>
2023-04-11 09:29:39 +0530

    Added solution - LeetHub

---
+++ Geek's Training - GFG/geeks-training.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+    private:
+    int find_max(vector<vector<int>>&points,int index,int last_task,vector<vector<int>>&dp){
+        if(index==0){
+            int maxi=0;
+            for(int i=0;i<3;i++){
+                if(i!=last_task){
+                    maxi=max(maxi,points[0][i]);
+                } 
+            }
+            return maxi;
+        }
+        if(dp[index][last_task]!=-1) return dp[index][last_task];
+        int maxi=0;
+        for(int i=0;i<3;i++){
+            if(last_task!=i){
+               int point=points[index][i]+find_max(points,index-1,i,dp);
+               maxi=max(maxi,point);
+            }
+        }
+        return  dp[index][last_task]=maxi;
+    }
+  public:
+    int maximumPoints(vector<vector<int>>& points, int n) {
+       vector < vector < int > > dp(n, vector < int > (4, -1));
+       return find_max(points,n-1,3,dp);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> points;
+        for (int i = 0; i < n; ++i) {
+            vector<int> temp;
+            for (int j = 0; j < 3; ++j) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            points.push_back(temp);
+        }
+
+        Solution obj;
+        cout << obj.maximumPoints(points, n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
